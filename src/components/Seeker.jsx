0a6a5241bc02cba53dd50869b3015af9bd5da3f9
AleVeron <alejoveron65@gmail.com>
2022-09-16 23:18:30 -0300

    best filter ever

--- src/components/Seeker.jsx
+++ src/components/Seeker.jsx
@@ -1,24 +1,31 @@
-import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Results from './Results';
+
 
 export default function Seeker() {
 
     const navigate = useNavigate()
-
+    const [result, setResult] = useState()
+    console.log(result);
 
     const handleChange = (e) => {
-        const key = e.target.value.trim()
+        const key = e.target.value.trim().toLowerCase()
+        setResult(key)
 
-        if(key.length != 0 && key.length > 4){
+        if (key.length !== 0 && key.length > 4) {
             navigate(`/results?keyword=${key}`)
-        }else{
+        } else {
             navigate("/list")
         }
     }
 
     return (
 
-        <input onKeyUp={handleChange} className="form-control" type="text" name="key" placeholder="Search Here" />
+        <div>
+            <input onKeyUp={handleChange} className="form-control" type="text" name="key" placeholder="Search Here" />
+        </div>
+
 
     )
 }
